1aa2bc4976735523892eaf32e9cc399a843597ad
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 04:02:42 +0100

    added customStyle colors to the heading

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 
 const Assignment = () => {
-    const date = new Date(2019, 1, 1, 11);
+    const date = new Date(2019, 1, 1, 18);
     const currentDate = date.getHours()
     let greeting;
 
+    const customStyle = {
+        color: ""
+    }
+
+
     if (currentDate < 12){
         greeting = "Good Morning";
+        customStyle.color = "green";
     } else if ( currentDate < 18) {
         greeting = "Good Afternoon";
+        customStyle.color = "red";
     } else{
         greeting = "Good Nite";
+        customStyle.color = "blue";
     }
+
 
   return (
     <div>
-        <h1 className='heading'>{greeting}</h1>
+        <h1 className='heading' style={customStyle}>{greeting}</h1>
     </div>
   )
 }
