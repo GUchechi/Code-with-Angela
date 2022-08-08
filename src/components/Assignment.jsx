10afb32e602d831727a387b3539e011e048cafcb
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 03:41:29 +0100

    added newdate()

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const Assignment = () => {
+    const date = new Date();
+    const currentDate = date.getHours()
+    console.log(currentDate)
+
   return (
     <div>Assignment</div>
   )
