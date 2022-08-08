281fb1966cab71fe93223d669b5a594c2a3b3e5c
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 10:34:26 +0100

    added a Lists component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Heading from './components/Heading';
+import Lists from './components/Lists';
 
 function App() {
   return(
     <div>
       <Heading />
+      <Lists />
     </div>
   );
 }
---
+++ src/components/Lists.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Lists = () => {
+  return (
+    <div>
+        <ul>
+            <li>Bacon</li>
+            <li>Jamen</li>
+            <li>Noodles</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Lists
