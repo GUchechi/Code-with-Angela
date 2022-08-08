9e0162e34b07092c212ac70acf003691c0bd8dd5
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 02:32:53 +0100

    styled h1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 function App() {
-  const fName = 'Angela Yu'
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-
+ 
   return (
-    <div className="App">
-      <h1>Created by {fName}</h1>
-      <p>CopyRight {year}</p>
+    <div>
+      <h1 className='heading'>Favorite Food</h1>
+      <ul>
+        <li>Rice</li>
+        <li>Pasta</li>
+        <li>Noodles</li>
+        <li>Vegetables</li>
+      </ul>
 
     </div>
   );
