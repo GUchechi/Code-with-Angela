68eb8727360638c0273a286679789297a1dc5036
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 01:35:04 +0100

    javascript expressions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
 function App() {
+  const name = 'Angela'
+  const number = 7
+
   return (
     <div className="App">
-    
+      <h1>Hello {name}!</h1>
+      <p>Your lucky number is {number}</p>
     </div>
   );
 }
