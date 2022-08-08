07fa972fd4af4d97d0ebadf45244be759b1ec64f
GUchechi <godswilluchendu7@gmail.com>
2022-08-08 03:36:27 +0100

    created a functional component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ function App() {
  
   return (
     <div>
-      <h1 className='heading'>Favorite Food</h1>
-      <ul>
-        <li>Rice</li>
-        <li>Pasta</li>
-        <li>Noodles</li>
-        <li>Vegetables</li>
-      </ul>
-
+     
     </div>
   );
 }
