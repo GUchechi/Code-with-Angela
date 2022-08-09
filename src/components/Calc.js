a8f57ca12c9ed38114b47b14026ab65053a88512
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 04:35:38 +0100

    added sub,multiply & divide func

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -2,6 +2,17 @@
     return n1 + n2
  }
 
+ const sub = (n1, n2) => {
+    return n1 - n2
+ }
+
+ const multiply = (n1, n2) => {
+    return n1 * n2
+ }
+
+ const divide = (n1, n2) => {
+    return n1 / n2
+ }
 
 
 
