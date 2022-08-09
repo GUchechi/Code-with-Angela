e73230b39a19b990352a6a56bb61237bcbf827bf
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 04:32:31 +0100

    imported add function

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import add from './Calc.js'
 
 const Lists = () => {
   return (
     <div>
         <ul>
-            <li>{Pi}</li>
+            <li>{add(1 , 2)}</li>
             <li></li>
             <li></li>
         </ul>
