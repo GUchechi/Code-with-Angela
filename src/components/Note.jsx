682ae0b518878e26fab6919455be6a8887ff7ab3
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 06:46:33 +0100

    imported note component into app.js

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Note = () => {
   return (
     <div className='note'>
-        
+        <h1>This is the title</h1>
+        <p>This is the content</p>
     </div>
   )
 }
