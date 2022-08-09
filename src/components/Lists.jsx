3cd062b21fad057095423ea8c6516acd05ced609
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 04:11:04 +0100

    imported double pi

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Pi from './Maths.js'
+import Pi,{doublePi} from './Maths.js'
 
 const Lists = () => {
   return (
     <div>
         <ul>
             <li>{Pi}</li>
-            <li>Jamen</li>
+            <li>{doublePi}</li>
             <li>Noodles</li>
         </ul>
     </div>
