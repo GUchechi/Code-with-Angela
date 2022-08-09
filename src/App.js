bdc85078ca8002ecf43209152a7a88790f094e5c
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 03:55:31 +0100

    added Assignment comp. into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Assignment from './components/Assignment';
 import Heading from './components/Heading';
 import Lists from './components/Lists';
 
 function App() {
   return(
     <div>
+      <Assignment />
       <Heading />
       <Lists />
     </div>
