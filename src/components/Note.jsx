bc09317fa9aab6f15b622c2c95bc4614ffc0a3c6
GUchechi <godswilluchendu7@gmail.com>
2022-08-09 06:54:00 +0100

    styled note h1

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ const Note = () => {
   return (
     <div className='note'>
         <h1>This is the title</h1>
-        <p>This is the content</p>
+        <p>This is the note content</p>
     </div>
   )
 }
