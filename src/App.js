98db5b8b5a88546587bb3155ecbc9b664486e5ca
GUchechi <godswilluchendu7@gmail.com>
2022-08-12 01:33:49 +0100

    looped contaccts

--- src/App.js
+++ src/App.js
@@ -5,31 +5,29 @@ import contacts from './components/Contact'
 // import Header from './components/Header';
 // import Note from './components/Note';
 
+
+const createCard = (contact) =>{
+  return (
+    <Card 
+      key={contact.name}
+      name={contact.name}
+      img={contact.imgURL}
+      telephone={contact.phone}
+      email={contact.email}
+    />
+  )
+}
+
 function App() {
   return(
     <div>
      {/* <Header />
      <Note />
      <Footer /> */} 
+
+     {contacts.map(createCard)}
     
-     <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+     
     </div>
   );
 }
